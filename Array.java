e40b82f507914d74ce53c3c6016c2658a3d081c5
balasaravanan2000 <balasaravanan5126@gmail.com>
2025-03-22 19:19:40 +0530

    codes

---
+++ Array.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class array{
+    public static void main(String args[]){
+        
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int[] num=new int[n];
+        for(int i=0;i<n;i++){
+            num[i]=scan.nextInt();
+
+         System.out.println(num[i]);
+
+        }
+
+
+
+
+
+    }
+}
