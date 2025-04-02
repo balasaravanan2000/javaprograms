b2255b701c64d6cb6b240a2e6cd9f18b6b949c84
Bala saravanan k <balasaravanan5126@gmail.com>
2025-04-02 18:48:11 +0530

    codes

---
+++ Squareroot.java
@@ -0,0 +1,12 @@
+public class Squareroot {
+    public static void main (String[] args){
+        int num=25;
+        int x=0;
+        for(int i=1;i*i<=num;i++){
+            x=i;
+        }
+        System.out.println(x);
+        }
+    }
+    
+
