983e06a906e61f8592e9e3ee4b3ee11e8ddaf81b
Bala saravanan k <balasaravanan5126@gmail.com>
2025-04-07 18:14:32 +0530

    code

---
+++ Find_length_of_string.java
@@ -0,0 +1,15 @@
+public class Find_length_of_string {
+    public static void main(String[] args) {
+        String s="alphabet";
+        char[] x=s.toCharArray();
+        int count=0;
+        for(int i:x){
+            count++;
+
+        }
+        System.out.println(count);
+
+
+    }
+    
+}
