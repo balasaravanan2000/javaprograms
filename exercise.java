5d10b65a30d83db9f954894fc244842ea24d879c
Balasaravanan2000 <balasaravanan5126@gmail.com>
2025-05-12 12:01:47 +0530

    pro

--- exercise.java
+++ exercise.java
@@ -4,24 +4,34 @@ public class exercise {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int arr[] = new int[5];
-        int temp = 0;
+        
         for (int i = 0; i < 5; i++) {
+            
             arr[i] = scan.nextInt();
         }
-
         for (int i = 0; i < 5; i++) {
-            for (int j = i+1; j < 5; j++) {
-               if( arr[i]>arr[j]){
-                   temp =arr[i];
-                   arr[i]=arr[j];
-                   arr[j]=temp;
-               }
+            int count = 1;
+            int repeat=0;
+            for (int j = 0; j < 5; j++) {
+                if (arr[i] == arr[j]) {
+                    repeat++;
+                    break;
+                }
+
             }
+            if(repeat==0){
+                for(int j=i+1;i<5;i++){
+                   if(arr[i]==arr[j]){
+                    count++;
+                   }
+                }
+            }
+           
+         System.out.println(arr[i]  + " occurs " + count + " times");
         }
-      for(int i=0;i<5;i++){
-        System.out.println();
-        System.out.print(arr[i]);
-      }
+
+
+    
     }
 
 }
